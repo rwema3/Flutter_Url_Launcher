aa1f41b00d3d42cee17df83ea7c21a13dd4d9c16
rwema3 <rwemadominik3@gmail.com>
2022-01-17 14:30:20 +0200

    application

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Fl',
+      title: 'F',
       theme: ThemeData(
         // This is the theme of your application.
         //
