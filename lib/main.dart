1b232deea31c1a55f9a9ea8c14b292fa5204b229
rwema3 <rwemadominik3@gmail.com>
2022-01-17 14:30:59 +0200

    application

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: '',
+      title: 'Rwema',
       theme: ThemeData(
         // This is the theme of your application.
         //
