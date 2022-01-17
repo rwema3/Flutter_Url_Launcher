3cb2faee82027ac5be34ff842e25fb115bbf0933
rwema3 <rwemadominik3@gmail.com>
2022-01-17 14:56:46 +0200

    FlutterApp

--- lib/main.dart
+++ lib/main.dart
@@ -27,11 +27,6 @@ class MyApp extends StatelessWidget {
 }
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // This class is the configuration for the state. It holds the values (in this
-
   final String title;
 
   @override
