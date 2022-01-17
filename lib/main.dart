2a00da5e2689060835863bac00a37fd330804501
rwema3 <rwemadominik3@gmail.com>
2022-01-17 14:55:15 +0200

    FlutterApp

--- lib/main.dart
+++ lib/main.dart
@@ -30,12 +30,7 @@ class MyHomePage extends StatefulWidget {
 
   // This widget is the home page of your application. It is stateful, meaning
   // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
   // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-
 
   final String title;
 
