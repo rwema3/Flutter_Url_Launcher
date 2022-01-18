a9bb1e9f9281fef0a089291f19540f9167e16abc
rwema3 <rwemadominik3@gmail.com>
2022-01-18 15:07:31 +0200

    FlutterApp

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,9 +1,6 @@
 // This is a basic Flutter widget test.
-//
 // To perform an interaction with a widget in your test, use the WidgetTester
 // utility that Flutter provides. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
