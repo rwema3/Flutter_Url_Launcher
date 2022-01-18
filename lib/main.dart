7cf80317e7a48af8e22a71edd0ceddec587dcef0
rwema3 <rwemadominik3@gmail.com>
2022-01-18 15:05:20 +0200

    FlutterApp

--- lib/main.dart
+++ lib/main.dart
@@ -61,8 +61,6 @@ class _MyHomePageState extends State<MyHomePage> {
           // Invoke "debug painting" (press "p" in the console, choose the
           // "Toggle Debug Paint" action from the Flutter Inspector in Android
           // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
           // Column has various properties to control how it sizes itself and
           // how it positions its children. Here we use mainAxisAlignment to
           // center the children vertically; the main axis here is the vertical
