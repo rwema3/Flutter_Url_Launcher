5fe670ea6df08b09308671b8ec8e777272dd8072
rwema3 <rwemadominik3@gmail.com>
2022-01-19 21:34:08 +0200

    FlutterApp

--- lib/main.dart
+++ lib/main.dart
@@ -52,8 +52,6 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           // Column is also layout widget. It takes a list of children and
           // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
           // Invoke "debug painting" (press "p" in the console, choose the
           // "Toggle Debug Paint" action from the Flutter Inspector in Android
           // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
