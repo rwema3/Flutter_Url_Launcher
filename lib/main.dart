59c46d8f8ee1b688bbaf15db657a26299aa5dc80
rwema3 <rwemadominik3@gmail.com>
2022-01-19 21:38:34 +0200

    FlutterApp

--- lib/main.dart
+++ lib/main.dart
@@ -46,14 +46,7 @@ class _MyHomePageState extends State<MyHomePage> {
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
         child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
+         // center the children vertically; the main axis here is the vertical
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Column(
