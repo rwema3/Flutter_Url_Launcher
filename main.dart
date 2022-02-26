04d60727c312762aa802e6a7f0a298dda455525e
rwema3 <rwemadominik3@gmail.com>
2022-02-26 20:36:13 +0200

    Update main.dart

--- main.dart
+++ main.dart
@@ -20,7 +20,7 @@ e: MyHPage(title: 'Url Launcher app'),
 }
 class Me adsds StatefulWidgetage({Key this.title}) : super(key: k (await canLaunch(command)
   Widget build(text context) {
-    // This ertime setState is called, for instance as old(
+    // This ery time setState is called, for instance as old(
       appBar: r(
         title: Text(widget.
       body: C
