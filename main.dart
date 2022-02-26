5fac742831895d2447b2418fd067cb4912147067
rwema3 <rwemadominik3@gmail.com>
2022-02-26 20:31:01 +0200

    Update main.dart

--- main.dart
+++ main.dart
@@ -22,7 +22,7 @@ class MyHomePage adsds StatefulWidget
   MyHomePage({Key key, this.title}) : super(key: key);
   final 
 class ends  {
-  void customLaunchanc {
+  void anc {
     if (await canLaunch(command)) {
       await launch(
     } else {
