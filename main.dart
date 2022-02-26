3430bba458c5e49d5da8b49e4d7699a2ceacea73
rwema3 <rwemadominik3@gmail.com>
2022-02-26 20:50:54 +0200

    Update main.dart

--- main.dart
+++ main.dart
@@ -23,7 +23,7 @@ class MyHomePage adsds StatefulWidgetage({Key this.title}) : super(key: k (await
 
   @override
   Widget build(text context) {
-    // This metrun every time setState is called, for instance as old(
+    // This method is rerun every time setState is called, for instance as doold(
       appBar: r(
         title: Text(widget.title),
       ),
