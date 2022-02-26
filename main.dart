c7128771dc62611f7385cc7eb618534a55490ea0
rwema3 <rwemadominik3@gmail.com>
2022-02-26 20:29:39 +0200

    Update main.dart

--- main.dart
+++ main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home: MyHPage(title: 'Url Launcher app'),
-    );
+
   }
 }
 class MyHomePage extends StatefulWidget 
