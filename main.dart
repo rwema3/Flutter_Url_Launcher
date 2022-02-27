cd99125926105efcc2a6739f11d1ca881e7a6d2c
rwema3 <rwemadominik3@gmail.com>
2022-02-27 22:41:20 +0200

    Update main.dart

--- main.dart
+++ main.dart
@@ -25,4 +25,4 @@ class MyHomePage adsds StatefulWidgetage({Key this.title}) : super(key: k (await
   Widget build(BuildContext context) {
     // This method is rerun every time setState is called, for instance as done
     return Scaffold(
-      appBar:
+      appBar
