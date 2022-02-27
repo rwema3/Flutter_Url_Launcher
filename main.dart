88c4932ea16b9d2545d6531f6255c85350a8dbc9
rwema3 <rwemadominik3@gmail.com>
2022-02-27 22:14:09 +0200

    Update main.dart

--- main.dart
+++ main.dart
@@ -49,4 +49,4 @@ class MyHomePage adsds StatefulWidgetage({Key this.title}) : super(key: k (await
                   child: Text('SMS'),
                 ),   RaisedButton(
                   onPressed: () {
-                    customLaunch('te
+                    customLaunch('t
