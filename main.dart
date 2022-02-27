c2d5320b9dda5111c4529d89b4baac0e38373864
rwema3 <rwemadominik3@gmail.com>
2022-02-27 23:09:41 +0200

    mainAxisAlignment: MainAxisAlignment.center,           children: <Widget>[             Column(               children: <Widget>[RaisedButton(                 onPressed: () {                   customLaunch(                       'mailto:rwemadominik3@gmail.com?subject=test%20subject&body=test%20body');                 },                 child: Text('Email'),               ),                 RaisedButton(

--- main.dart
+++ main.dart
@@ -43,3 +43,4 @@ class MyHomePage adsds StatefulWidgetage({Key this.title}) : super(key: k (await
                 child: Text('Email'),
               ),
                 RaisedButton(
+                  
