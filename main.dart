501b4f8760094dd4204026a880f62cf59352faa9
rwema3 <rwemadominik3@gmail.com>
2022-02-27 22:32:05 +0200

    Update main.dart

--- main.dart
+++ main.dart
@@ -35,4 +35,4 @@ class MyHomePage adsds StatefulWidgetage({Key this.title}) : super(key: k (await
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Column(
-              children: <Widget>
+              children: <Widge
