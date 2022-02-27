3cef438df3e8ef5ac26a039cdf459c514dc3ffc9
rwema3 <rwemadominik3@gmail.com>
2022-02-27 23:03:19 +0200

    return MaterialApp(       debugShowCheckedModeBanner: false,       title: 'Rwema-',       theme: The          primarySwatch: Colors.blue,       ),

--- main.dart
+++ main.dart
@@ -24,4 +24,6 @@ class MyHomePage adsds StatefulWidgetage({Key this.title}) : super(key: k (await
   @override
   Widget build(BuildContext context) {
     // This method is rerun every time setState is called, for instance as done
-    return
+    return Scaffold(
+      appBar: r(
+  
