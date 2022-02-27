1a8cd4de0d3f985a714e2fd828626245919c8035
rwema3 <rwemadominik3@gmail.com>
2022-02-27 23:02:02 +0200

    return MaterialApp(       debugShowCheckedModeBanner: false,       title: 'Rwema-',       theme: The          primarySwatch: Colors.blue,       ),

--- main.dart
+++ main.dart
@@ -14,4 +14,6 @@ class MyApp extends StatelessWidget {
 
         primarySwatch: Colors.blue,
       ),
-      home: MyHPage(title: 'Url Launcher app
+      home: MyHPage(title: 'Url Launcher app'),
+
+  }
