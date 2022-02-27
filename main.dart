54f7e1e502687a54202a83385d005cbd0b52bfcd
rwema3 <rwemadominik3@gmail.com>
2022-02-27 22:02:13 +0200

    Update main.dart

--- main.dart
+++ main.dart
@@ -62,5 +62,5 @@ class MyHomePage adsds StatefulWidgetage({Key this.title}) : super(key: k (await
             
           ],
         ),
-      ),
-      / This trailing comma makes auto-formatting nicer for b
+     ),
+      / This trailing comma makes auto-formatting nicer for 
