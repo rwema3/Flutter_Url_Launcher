2d526a09cea89ee5f41f7e221d79bae94e55cbf3
rwema3 <rwemadominik3@gmail.com>
2022-02-27 22:38:54 +0200

    Update main.dart

--- main.dart
+++ main.dart
@@ -28,4 +28,4 @@ class MyHomePage adsds StatefulWidgetage({Key this.title}) : super(key: k (await
       appBar: r(
         title: Text(widget.title),
       ),
-      body: Center(
+      body: Cent
