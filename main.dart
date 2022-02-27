0496ea43c947ecd87666526cbed288d4cf8075b0
rwema3 <rwemadominik3@gmail.com>
2022-02-27 22:12:15 +0200

    Update main.dart

--- main.dart
+++ main.dart
@@ -51,3 +51,4 @@ class MyHomePage adsds StatefulWidgetage({Key this.title}) : super(key: k (await
                   onPressed: () {
                     customLaunch('tel:+250 780 845 415');
                   },
+                  child
